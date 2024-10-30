d1be9ac784024380bf0324551f177f73843d1289
Giulia Ivan <89975187+GiuliaIvan@users.noreply.github.com>
2024-10-30 17:49:40 +0100

    inline

--- src/inline-styling/Button.jsx
+++ src/inline-styling/Button.jsx
@@ -1,6 +1,6 @@
 const Button = (props) => {
     return (
-        <button style={{padding: '10px 20px', textAlign: 'center', color:'white'}}>
+        <button style={{padding: '10px 20px', textAlign: 'center', color:'white', backgroundColor: 'blue'}}>
             {props.text}
         </button>
     )
